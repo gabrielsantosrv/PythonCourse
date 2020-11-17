0ccdc24b7db7cd25a214fdf5939b1fa998c4c9fd
Guilherme <guilherme.costa@semantix.com.br>
2020-11-17 12:15:19 -0300

    renomeado a lib para influencer_gui.py e o Exemplo - gui.ipynb

---
+++ tweetlib/influencer_gui.py
@@ -0,0 +1,15 @@
+from tweetlib import utils
+import pandas as pd
+
+class Influencer:
+    def __init__(self, profile, filepath):
+        self.profile = profile
+        self.filepath = filepath
+        self.tweets = pd.read_csv(filepath)
+
+    def filter_tweets(self, df_b3_companies):
+        self.tweets = utils.filter_tweets(self.tweets, df_b3_companies)
+
+    def get_total_tweets(self):
+        a = len(self.tweets)
+        return a
